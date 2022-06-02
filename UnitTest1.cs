51289bae1c449b1772db32c6c03880608be4009e
Marc Steen Fordsmand <marc@fordsmand.dk>
2022-06-02 13:51:03 +0200

    Lavede nye test til de nye metoder fra Blackjack projektet.

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -79,11 +79,68 @@ namespace TestProjectBlackJack
             //Arrange
             Player player = new Player("Marc", 0);
             Card card = new Card("King", 10, "Hearts");
-            Card card2 = new Card("Ace", 11, "Clovers");
+            Card card2 = new Card("10", 10, "Spades");
+            Card card3 = new Card("Ace", 1, "Clovers");
 
             //Act
             player.ReceivePoints(card);
             player.ReceivePoints(card2);
+            player.ReceivePoints(card3);
+
+            //Assert
+            Assert.AreEqual(21, player.TotalPoints);
+        }
+
+        /// <summary>
+        /// Testing if the player's total points do become 21 if the card "Joker" is picked.
+        /// </summary>
+        [TestMethod]
+        public void TestPickingJoker()
+        {
+            //Arrange
+            Player player = new Player("Marc", 0);
+            Card card = new Card("Joker", 0, "");
+
+            //Act
+            card.PickingJoker(card, player);
+
+            //Assert
+            Assert.AreEqual(21, player.TotalPoints);
+        }
+
+        /// <summary>
+        /// Test to see if the player's total points increases by 10, if an Ace is picked.
+        /// </summary>
+        [TestMethod]
+        public void TestPickingAce()
+        {
+            //Arrange
+            Player player = new Player("Marc", 10);
+            Card card = new Card("Ace", 1, "Hearts");
+
+            //Act
+            card.PickingAce(card.Name, player);
+            player.ReceivePoints(card);
+
+            //Assert
+            Assert.AreEqual(21, player.TotalPoints);
+        }
+
+        /// <summary>
+        /// Tests the same as TestPickingAce, but this one is more like a real game of blackjack.
+        /// </summary>
+        [TestMethod]
+        public void TestPickingAceAndWin()
+        {
+            //Arrange
+            Player player = new Player("Marc", 0);
+            Card card = new Card("King", 10, "Clover");
+            Card cardA = new Card("Ace", 1, "Hearts");
+
+            //Act
+            cardA.PickingAce(cardA.Name, player);
+            player.ReceivePoints(card);
+            player.ReceivePoints(cardA);
 
             //Assert
             Assert.AreEqual(21, player.TotalPoints);
